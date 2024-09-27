a3ff04a8ac24539d3093e9aafddaa9b8e6ca7bc3
tobias1918 <tobilp86@gmail.com>
2024-09-27 11:58:46 -0300

    Pequeño cambio en roles

--- src/app/features/page.component.ts
+++ src/app/features/page.component.ts
@@ -17,11 +17,24 @@ export class PageComponent  {
   }
 
   constructor(private router: Router) {} // Inyecta el Router
+  userId: number | null = null;
+
+  ngOnInit(): void {
+    this.getUserId();
+  }
+
+
+  getUserId(): void {
+    const userIdString = localStorage.getItem('userId');
+    this.userId = userIdString ? parseInt(userIdString, 10) : null;
+    console.log('User ID:', this.userId);
+  }
 
   cerrarSesion(): void {
     // Eliminar el token y el userId del localStorage
     localStorage.removeItem('token');
     localStorage.removeItem('userId');
+    
 
     // Redirigir al usuario a la página de signup
     this.router.navigate(['/signup']);
